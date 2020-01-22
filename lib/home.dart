ca1be55343fd1b701857df5ed29f9ad60b4ee592
BMarx981 <b.marx981@gmail.com>
2020-01-22 14:08:52 -0500

    "Update bottom nav bar to navigate between tabs in different files"

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -6,6 +9,66 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                height: 175,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.grey.withOpacity(0.75), BlendMode.overlay),
+                    fit: BoxFit.fitWidth,
+                    image: NetworkImage(
+                        "https://biketothebeach.org/wp-content/uploads/2018/02/W3A8138_web.jpg"),
+                  ),
+                ),
+              ),
+            ),
+            mainTextContent(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container mainTextContent() {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Row(
+          children: <Widget>[
+            Text(
+              "Bike for ",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.bold),
+            ),
+            RotateAnimatedTextKit(
+              isRepeatingAnimation: true,
+              text: [
+                "the challenge",
+                "fun",
+                "friends",
+                "Autisim",
+              ],
+              textStyle: TextStyle(
+                  color: Color(0xFFFED344),
+                  fontSize: 30,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.start,
+              alignment: AlignmentDirectional.bottomCenter,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'rides.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,7 +28,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Widget> _children = [RidesTab()];
+  final List<Widget> _children = [Home(), RidesTab()];
   int _currentIndex = 0;
 
   void onTabTapped(int index) {
@@ -62,65 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  height: 175,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      colorFilter: ColorFilter.mode(
-                          Colors.grey.withOpacity(0.75), BlendMode.overlay),
-                      fit: BoxFit.fitWidth,
-                      image: NetworkImage(
-                          "https://biketothebeach.org/wp-content/uploads/2018/02/W3A8138_web.jpg"),
-                    ),
-                  ),
-                ),
-              ),
-              mainTextContent(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Container mainTextContent() {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Row(
-          children: <Widget>[
-            Text(
-              "Bike for ",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontFamily: "Roboto",
-                  fontWeight: FontWeight.bold),
-            ),
-            RotateAnimatedTextKit(
-              isRepeatingAnimation: true,
-              text: [
-                "the challenge",
-                "fun",
-                "friends",
-                "Autisim",
-              ],
-              textStyle: TextStyle(
-                  color: Color(0xFFFED344),
-                  fontSize: 30,
-                  fontFamily: "Roboto",
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.start,
-              alignment: AlignmentDirectional.bottomCenter,
-            ),
-          ],
-        ),
+        body: _children[_currentIndex],
       ),
     );
   }
