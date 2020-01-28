6694146089e63c191fc00a206c4338b743fe0e90
BMarx981 <b.marx981@gmail.com>
2020-01-27 21:33:18 -0500

    "Add date to ride tile"

--- lib/rides.dart
+++ lib/rides.dart
@@ -22,36 +22,43 @@ class _RidesTabState extends State<RidesTab> {
         child: ListView(
           children: <Widget>[
             RidesCity(
+              theDate: "February 15, 2020",
               city: "Florida",
               imageLink:
                   "https://i2.wp.com/biketothebeach.org/wp-content/uploads/2013/08/Miami.jpg?zoom=2&w=1200&ssl=1",
             ),
             RidesCity(
+              theDate: "June 5, 2020",
               city: "New York",
               imageLink:
                   "https://i0.wp.com/biketothebeach.org/wp-content/uploads/2013/08/9489311734_790298c1cb_z-e1485987040780.jpg?zoom=2&w=1200&ssl=1",
             ),
             RidesCity(
+              theDate: "July 31, 2020",
               city: "Washington DC",
               imageLink:
                   "https://i0.wp.com/biketothebeach.org/wp-content/uploads/2013/08/13815683164_d031d40350_z.jpg?zoom=2&w=1200&ssl=1",
             ),
             RidesCity(
+              theDate: "July 31, 2020",
               city: "Maryland",
               imageLink:
                   "https://i1.wp.com/biketothebeach.org/wp-content/uploads/2013/08/9550448611_7baa796d72_z-e1485987247873.jpg?zoom=2&w=1200&ssl=1",
             ),
             RidesCity(
+              theDate: "September 2020",
               city: "New England",
               imageLink:
                   "https://i1.wp.com/biketothebeach.org/wp-content/uploads/2013/08/15042356956_82e0cffd53_z.jpg?zoom=2&w=1200&ssl=1",
             ),
             RidesCity(
+              theDate: "Fall 2020",
               city: "Texas",
               imageLink:
                   "https://i0.wp.com/biketothebeach.org/wp-content/uploads/2013/08/6883513430_9f4c202325_z.jpg?zoom=2&w=1200&ssl=1",
             ),
             RidesCity(
+              theDate: "May 9, 2020",
               city: "Bay Area",
               imageLink:
                   "https://i0.wp.com/biketothebeach.org/wp-content/uploads/2019/01/Bay-Area-Pic-400x200.jpg?zoom=2&resize=400%2C200&ssl=1",
@@ -66,8 +73,9 @@ class _RidesTabState extends State<RidesTab> {
 class RidesCity extends StatelessWidget {
   final String city;
   final String imageLink;
+  final String theDate;
   final textOutline = Color(0xff075c93);
-  RidesCity({this.city, this.imageLink});
+  RidesCity({this.theDate, this.city, this.imageLink});
 
   @override
   Widget build(BuildContext context) {
@@ -84,36 +92,48 @@ class RidesCity extends StatelessWidget {
             image: NetworkImage(imageLink),
           ),
         ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              city,
-              style: TextStyle(
-                shadows: [
-                  Shadow(
-                      // bottomLeft
-                      offset: Offset(-1.5, -1.5),
-                      color: textOutline),
-                  Shadow(
-                      // bottomRight
-                      offset: Offset(1.5, -1.5),
-                      color: textOutline),
-                  Shadow(
-                      // topRight
-                      offset: Offset(1.5, 1.5),
-                      color: textOutline),
-                  Shadow(
-                      // topLeft
-                      offset: Offset(-1.5, 1.5),
-                      color: textOutline),
-                ],
-                color: Color(0xFFFED344),
-                fontFamily: "Roboto",
-                fontSize: 50.0,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                city,
+                style: TextStyle(
+                  shadows: [
+                    Shadow(
+                        // bottomLeft
+                        offset: Offset(-1.5, -1.5),
+                        color: textOutline),
+                    Shadow(
+                        // bottomRight
+                        offset: Offset(1.5, -1.5),
+                        color: textOutline),
+                    Shadow(
+                        // topRight
+                        offset: Offset(1.5, 1.5),
+                        color: textOutline),
+                    Shadow(
+                        // topLeft
+                        offset: Offset(-1.5, 1.5),
+                        color: textOutline),
+                  ],
+                  color: Color(0xFFFED344),
+                  fontFamily: "Roboto",
+                  fontSize: 50.0,
+                ),
               ),
             ),
-          ),
+            SizedBox(height: 20),
+//            Divider(
+//              color: Color(0xFF075c93),
+//              height: 30.0,
+//            ),
+            Text(
+              theDate,
+              style: TextStyle(
+                  color: Color(0xFFFED344), fontFamily: "Roboto", fontSize: 20),
+            ),
+          ],
         ),
       ),
     );
